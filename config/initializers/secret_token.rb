05bf4969310c2768e94643826969cc317a9b3f63
Yup Tan <seventanyb@gmail.com>
2013-10-10 19:53:53 +0800

    rails new iulsf -T -d mysql
    rails generate controller welcome home

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iulsf::Application.config.secret_key_base = '682e0a26b063e844b21692ee1f5e1fe9ecf813ba6c0609e5c52533d09cbcadfebe01b1658d888295f0734c425c9ec1d973b6545f812c4bd98e917599242a165b'
